54b6028a2eb7fdb03e33be73a36ecdd1008bd150
Pablo <paablovelasco@gmail.com>
2020-11-16 22:34:00 +0100

    Added login system [Not finished]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/shared/login/login.component';
 
-
+import { AuthGuard } from './_helpers/auth.guard';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { fakeBackendProvider } from './_helpers/fake-backend';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { LoginComponent } from './components/shared/login/login.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HomeComponent,
 		NavbarComponent,
-		FooterComponent
+		FooterComponent,
+		LoginComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
-	],
-	providers: [],
+		AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
